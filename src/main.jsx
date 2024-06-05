26f50c82e10b838ea35ba52d86f9bd77c77902a1
Huu Hien <tranlehuuhien32@gmail.com>
2024-06-05 14:42:25 +0700

    router

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './App.jsx';
-import './index.css';
-import { PersistGate } from 'redux-persist/integration/react'
-import { persistor, store } from './redux/store.js';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor, store } from "./redux/store.js";
+import { Provider } from "react-redux";
 
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-
+ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter>
-          <App/>
-      </BrowserRouter>
+      <App />
     </PersistGate>
-  </Provider>,
+  </Provider>
 );
-
