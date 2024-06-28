d3c50520128c4d0ca7d563665995f0bb8ae4c3e0
minhhieu.nguyen250903@gmail.com <minhhieu.nguyen250903@gmail.com>
2024-06-28 14:24:09 +0700

    fix page history

--- src/pages/staffs/History.jsx
+++ src/pages/staffs/History.jsx
@@ -1,21 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Table, { HistoryHeadTable } from '../../components/Table';
 import { historyData } from '../../data/data';
 import Header from '../../component/header/Header';
+import { Icon } from "@iconify/react/dist/iconify.js";
 import './History.css';
 
+const ITEMS_PER_PAGE = 5; // Number of items to display per page
+
 export default function History() {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  // Calculate total pages
+  const totalPages = Math.ceil(historyData.length / ITEMS_PER_PAGE);
+
+  // Get the current page data
+  const currentData = historyData.slice(
+    (currentPage - 1) * ITEMS_PER_PAGE,
+    currentPage * ITEMS_PER_PAGE
+  );
+
+  const handlePrevPage = () => {
+    if (currentPage > 1) setCurrentPage(currentPage - 1);
+  };
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) setCurrentPage(currentPage + 1);
+  };
+
   return (
     <div>
       <Header page={"HISTORY"} />
 
       <div className="container">
-        <Table headTable={HistoryHeadTable} data={historyData} />
+        <Table headTable={HistoryHeadTable} data={currentData} />
       </div>
 
-      <div className="button-group">
-        <button className="cancel-button">CANCEL</button>
-        <button className="print-button">PRINT</button>
+      <div className="pagination-controls flex items-center justify-center text-xl mt-3">
+        <Icon icon="teenyicons:left-solid" onClick={handlePrevPage} />
+        <span className="mx-5">{currentPage} OF {totalPages}</span>
+        <Icon icon="teenyicons:right-solid" onClick={handleNextPage} />
+      </div>
+
+      <div className="button-group flex justify-center mt-5">
+        <button className="cancel-button px-10 py-3 rounded-lg bg-neutral-600 text-white mr-3 flex items-center">
+          CANCEL
+        </button>
+        <button className="print-button px-7 py-3 rounded-lg bg-blue-400 text-white">
+          PRINT
+        </button>
       </div>
     </div>
   );
