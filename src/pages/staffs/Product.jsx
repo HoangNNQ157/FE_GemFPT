3e0e255f08a2fbe7976e7bc59361193f7acb06d0
minhhieu.nguyen250903@gmail.com <minhhieu.nguyen250903@gmail.com>
2024-06-28 14:12:45 +0700

    delete page arrow

--- src/pages/staffs/Product.jsx
+++ src/pages/staffs/Product.jsx
@@ -7,6 +7,9 @@ import './Product.css';
 
 export default function Product() {
   return (
+
+    
+
     <div>
     <Header page={"PRODUCT"} />
 
@@ -14,9 +17,7 @@ export default function Product() {
       <Table headTable={ProductHeadTable} data={productData} />
       <div className=" flex items-center justify-center text-xl">
         <Icon icon="teenyicons:left-solid" />{" "}
-        <select className="px-5 bg-white mx-5">
-          <option>1</option>
-        </select>{" "}
+        
         <span className="mx-5">1 OF 10</span>{" "}
         <Icon icon="teenyicons:right-solid" />
       </div>
