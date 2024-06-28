3d9a4820fc91c57c3917ab37502f1d34b77f4716
minhhieu.nguyen250903@gmail.com <minhhieu.nguyen250903@gmail.com>
2024-06-28 14:42:43 +0700

    add data

--- src/data/data.js
+++ src/data/data.js
@@ -128,9 +128,48 @@ export const productData = [
         price:"15.763.000đ",
         quantity:1,
     },
-   
-
-
+    {
+        image:jewerl,
+        productName:"18K Italian White Gold Necklace",
+        productId:"0000W000583",
+        price:"15.763.000đ",
+        quantity:1,
+    },
+    {
+        image:jewerl,
+        productName:"18K Italian White Gold Necklace",
+        productId:"0000W000583",
+        price:"15.763.000đ",
+        quantity:1,
+    },
+    {
+        image:jewerl,
+        productName:"18K Italian White Gold Necklace",
+        productId:"0000W000583",
+        price:"15.763.000đ",
+        quantity:1,
+    },
+    {
+        image:jewerl,
+        productName:"18K Italian White Gold Necklace",
+        productId:"0000W000583",
+        price:"15.763.000đ",
+        quantity:1,
+    },
+    {
+        image:jewerl,
+        productName:"18K Italian White Gold Necklace",
+        productId:"0000W000583",
+        price:"15.763.000đ",
+        quantity:1,
+    },
+    {
+        image:jewerl,
+        productName:"18K Italian White Gold Necklace",
+        productId:"0000W000583",
+        price:"15.763.000đ",
+        quantity:1,
+    }
 ]
 
 export const historyData = [
