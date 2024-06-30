dbd819483bc216bd13b81df7aa46924de6a8580e
minhhieu.nguyen250903@gmail.com <minhhieu.nguyen250903@gmail.com>
2024-06-30 15:05:58 +0700

    add option

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { blankAvatar } from "../../data/data";
 import { Icon } from "@iconify/react/dist/iconify.js";
 import "./Header.css";
 
 export default function Header({ page }) {
+  const [sortDropdownVisible, setSortDropdownVisible] = useState(false);
+
+  const toggleSortDropdown = () => {
+    setSortDropdownVisible(!sortDropdownVisible);
+  };
+
   return (
     <div>
       <div className="header-container">
@@ -16,11 +22,18 @@ export default function Header({ page }) {
               className="header-search-input"
               placeholder="SEARCH"
             />
-            <div className="header-sort-by">
+            <div className="header-sort-by" onClick={toggleSortDropdown}>
               <div>SORT BY</div> 
               <Icon className="icon-down" icon="teenyicons:down-solid" />
+              {sortDropdownVisible && (
+                <div className="sort-dropdown">
+                  <div className="sort-option">Product</div>
+                  <div className="sort-option">ID</div>
+                  <div className="sort-option">Name</div>
+                </div>
+              )}
             </div>
-            </div>
+          </div>
         </div>
         <div className="header-right">
           <div className="header-profile">
@@ -41,4 +54,3 @@ export default function Header({ page }) {
     </div>
   );
 }
-
