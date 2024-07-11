2053482d35d8a549cd5a27a878fa961af3844c88
Huu Hien <tranlehuuhien32@gmail.com>
2024-07-11 13:13:36 +0700

    form discount +metal

---
+++ src/components/TableManager/modal/RequestModal.jsx
@@ -0,0 +1,93 @@
+import React, { useState } from "react";
+import { Button, Modal, Form, Input } from "antd";
+import { requestDiscount } from "../../service/discount";
+import { toast } from "react-toastify";
+
+const RequestModal = ({
+    isModalVisible,
+    setIsModalVisible,
+    customerName,
+    customerPhone,
+}) => {
+    const [form] = Form.useForm();
+
+    const showModal = () => {
+        setIsModalVisible(true);
+    };
+
+    const handleOk = () => {
+        form.validateFields()
+            .then(async (values) => {
+                const data = await requestDiscount({
+                    customerName,
+                    customerPhone,
+                    discountReason: values.description,
+                    requestedDiscount: values.discount,
+                });
+                if (data.data) {
+                    toast.success("Create request discount success!");
+                }
+                setIsModalVisible(false);
+            })
+            .catch((info) => {
+                console.log("Validate Failed:", info);
+            });
+    };
+
+    const handleCancel = () => {
+        setIsModalVisible(false);
+    };
+
+    return (
+        <div>
+            <Modal
+                title="REQUEST"
+                visible={isModalVisible}
+                onOk={handleOk}
+                onCancel={handleCancel}
+                footer={[
+                    <Button
+                        key="search"
+                        danger
+                        type="default"
+                        onClick={handleCancel}
+                    >
+                        CANCEl
+                    </Button>,
+                    <Button key="submit" type="primary" onClick={handleOk}>
+                        SUBMIT
+                    </Button>,
+                ]}
+            >
+                <Form form={form} layout="vertical">
+                    <Form.Item
+                        name="discount"
+                        label="DISCOUNT:"
+                        rules={[
+                            {
+                                required: true,
+                                message: "Please input the discount!",
+                            },
+                        ]}
+                    >
+                        <Input placeholder="Discount" />
+                    </Form.Item>
+                    <Form.Item
+                        name="description"
+                        label="DESCRIPTION:"
+                        rules={[
+                            {
+                                required: true,
+                                message: "Please input the description!",
+                            },
+                        ]}
+                    >
+                        <Input placeholder="Description" />
+                    </Form.Item>
+                </Form>
+            </Modal>
+        </div>
+    );
+};
+
+export default RequestModal;
---
+++ src/components/TableManager/modal/RespondForm.jsx
@@ -0,0 +1,73 @@
+import { Button, Form, Input, Modal, Select } from "antd";
+import React, { useEffect } from "react";
+
+const RespondForm = ({ visible, onCancel, onSave, idResponse }) => {
+    const [form] = Form.useForm();
+
+    useEffect(() => {
+        if (visible) {
+            form.resetFields();
+        }
+    }, [visible]);
+
+    const handleOk = () => {
+        form.validateFields()
+            .then((values) => {
+                form.resetFields();
+                onSave(values);
+            })
+            .catch((info) => {
+                console.log("Validate Failed:", info);
+            });
+    };
+
+    return (
+        <Modal
+            title={"Custom Form Modal"}
+            visible={visible}
+            onOk={handleOk}
+            onCancel={onCancel}
+            width={800}
+        >
+            <Form form={form} layout="vertical" name="custom_form_modal">
+                <Form.Item
+                    name="managerResponse"
+                    label="Manager Response"
+                    rules={[
+                        {
+                            required: true,
+                            message: "Please input the manager response!",
+                        },
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    name="approved"
+                    label="Approved"
+                    rules={[
+                        {
+                            required: true,
+                            message:
+                                "Please select whether it's approved or not!",
+                        },
+                    ]}
+                >
+                    <Select>
+                        <Select.Option value={true}>True</Select.Option>
+                        <Select.Option value={false}>False</Select.Option>
+                    </Select>
+                </Form.Item>
+                <Form.Item name="discountRequestId" label="Discount Request ID">
+                    <Input
+                        defaultValue={idResponse}
+                        value={idResponse}
+                        disabled
+                    />
+                </Form.Item>
+            </Form>
+        </Modal>
+    );
+};
+
+export default RespondForm;
