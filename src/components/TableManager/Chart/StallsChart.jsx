dbb0733e8be6de1f58339d270bccf89d348f2bed
Huu Hien <tranlehuuhien32@gmail.com>
2024-07-11 13:39:11 +0700

    stall

---
+++ src/components/TableManager/Chart/StallsChart.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import {
+    BarChart,
+    Bar,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    Legend,
+    ResponsiveContainer,
+} from "recharts";
+
+const StallsChart = ({ data }) => {
+    return (
+        <ResponsiveContainer width="100%" height={400}>
+            <BarChart
+                width={500}
+                height={300}
+                data={data}
+                margin={{
+                    top: 20,
+                    right: 30,
+                    left: 20,
+                    bottom: 5,
+                }}
+            >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="stallsName" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Bar
+                    dataKey="totalRevenue"
+                    fill="#8884d8"
+                    name="Total Revenue"
+                />
+            </BarChart>
+        </ResponsiveContainer>
+    );
+};
+
+export default StallsChart;
