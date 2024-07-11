74a73ff9d094f323ef7f4ebe0e30c651b146204c
HoangNNQ157 <hoangnguyen157203@gmail.com>
2024-07-11 14:41:00 +0700

    revenue page

---
+++ src/service/revenue.js
@@ -0,0 +1,55 @@
+import api from "../config/axios";
+import Cookies from "js-cookie";
+const TOKEN = Cookies.get("token");
+const headers = {
+    "Content-Type": "application/json",
+    accept: "*/*",
+    Authorization: `Bearer ${TOKEN}`,
+};
+
+const getRevenueDataMonth = () => {
+    return api.get("monthly-revenue-data", { headers: headers });
+};
+const getRevenueDataYear = () => {
+    return api.get("yearly-revenue-data", { headers: headers });
+};
+const getRevenueStall = () => {
+    return api.get("revenue/stalls", { headers: headers });
+};
+const getRevenueCashiers = () => {
+    return api.get("revenue/cashiers", { headers: headers });
+};
+const getRevenueYearCashiersById = ({ cashierId, year }) => {
+    return api.get(`yearly-revenue/staff/${cashierId}?year=${year}`, {
+        headers: headers,
+    });
+};
+const getRevenueMothCashiersById = ({ cashierId, yearMonth }) => {
+    return api.get(
+        `monthly-revenue/staff/${cashierId}?yearMonth=${yearMonth}`,
+        {
+            headers: headers,
+        }
+    );
+};
+const getRevenueYearStallById = ({ stallId, year }) => {
+    return api.get(`yearly-revenue/${stallId}?year=${year}`, {
+        headers: headers,
+    });
+};
+const getRevenueMothStallById = ({ stallId, yearMonth }) => {
+    return api.get(`monthly-revenue/${stallId}?yearMonth=${yearMonth}`, {
+        headers: headers,
+    });
+};
+
+export {
+    getRevenueDataMonth,
+    getRevenueDataYear,
+    getRevenueCashiers,
+    getRevenueStall,
+    getRevenueYearStallById,
+    getRevenueMothStallById,
+    getRevenueMothCashiersById,
+    getRevenueYearCashiersById,
+};
